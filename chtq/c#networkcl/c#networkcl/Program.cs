0a9f88abe32c483b53e56c20238dca1fa44500ca
GOSUwat <78549782+GOSUwat@users.noreply.github.com>
2023-03-23 14:32:26 +0300

    test2

--- chtq/c#networkcl/c#networkcl/Program.cs
+++ chtq/c#networkcl/c#networkcl/Program.cs
@@ -1,30 +1,18 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Text;
 
-internal class Program
+using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+try
 {
-    private static async Task Main(string[] args)
-    {
-        //string ip = "192.168.50.249";
-        var port = 80;
-        IPAddress ip = new IPAddress(new byte[] { 192, 168, 50, 19 });
-        IPEndPoint ep = new IPEndPoint(ip, port);
-        using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        try
-        {
-            await socket.ConnectAsync(ep);
-            string message = "aaa";
-            var messageBytes = Encoding.UTF8.GetBytes(message);
-            int sended = await socket.SendAsync(messageBytes, SocketFlags.None);
-            Console.WriteLine($"Отправил {sended}");
-
-        }
-        catch (SocketException ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-
-    }
+    await socket.ConnectAsync(IPAddress.Any, 8888);
+    Console.WriteLine($"Подключился {socket.RemoteEndPoint}");
 }
+catch(SocketException ex)
+{
+    Console.WriteLine(ex.ToString());   
+}
+
+
--- chtq/c#networkserv/c#networkserv/Program.cs
+++ chtq/c#networkserv/c#networkserv/Program.cs
@@ -3,27 +3,12 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-internal class Program
-{
-    private static async Task Main(string[] args)
-    {
-        //string ip = "192.168.50.249";
-        var port = 80;
-        IPAddress ip = new IPAddress(new byte[] { 192, 168, 50, 19 });
-        IPEndPoint ep = new IPEndPoint(ip, port);
-        using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        try
-        {
-            var recive = new byte[512];
-            var bytes = await socket.ReceiveAsync(recive, SocketFlags.None);
-            string response = Encoding.UTF8.GetString(recive, 0, bytes);
-            Console.WriteLine(response);
-
-        }
-        catch (SocketException ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-
-    }
-}
+var port = 8888;
+IPEndPoint bind = new IPEndPoint(IPAddress.Any, port);
+using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+socket.Bind(bind);
+Console.WriteLine(socket.LocalEndPoint);
+socket.Listen(1000);
+Console.WriteLine("Ждем коннектов");
+using Socket client = await socket.AcceptAsync();
+Console.WriteLine($"Кто то подключился {client.RemoteEndPoint}");
