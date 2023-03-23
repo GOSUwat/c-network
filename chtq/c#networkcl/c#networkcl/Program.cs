ba4720d1298e3c75a2edb2c56a05b17268255762
GOSUwat <78549782+GOSUwat@users.noreply.github.com>
2023-03-23 14:12:20 +0300

    test

---
+++ chtq/c#networkcl/c#networkcl/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        //string ip = "192.168.50.249";
+        var port = 80;
+        IPAddress ip = new IPAddress(new byte[] { 192, 168, 50, 19 });
+        IPEndPoint ep = new IPEndPoint(ip, port);
+        using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        try
+        {
+            await socket.ConnectAsync(ep);
+            string message = "aaa";
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            int sended = await socket.SendAsync(messageBytes, SocketFlags.None);
+            Console.WriteLine($"Отправил {sended}");
+
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+}
---
+++ chtq/c#networkserv/c#networkserv/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        //string ip = "192.168.50.249";
+        var port = 80;
+        IPAddress ip = new IPAddress(new byte[] { 192, 168, 50, 19 });
+        IPEndPoint ep = new IPEndPoint(ip, port);
+        using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        try
+        {
+            var recive = new byte[512];
+            var bytes = await socket.ReceiveAsync(recive, SocketFlags.None);
+            string response = Encoding.UTF8.GetString(recive, 0, bytes);
+            Console.WriteLine(response);
+
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+}
